bea1678e2e73245a55fa4140106c5c3be7173716
Eric Roy <emroy@fs03.inband>
2023-02-23 18:29:06 -0500

    add erics contribution

--- utils.py
+++ utils.py
@@ -1,6 +1,1 @@
-def mask_ocean(da):
-    """Mask ocean areas"""
-    land = regionmask.defined_regions.natural_earth_v5_0_0.land_110
-    mask = land.mask(da, lon_name="lon", lat_name="lat")
-    da_new = da.where(~np.isnan(mask))
-    return da_new
+# Eric's contribution
